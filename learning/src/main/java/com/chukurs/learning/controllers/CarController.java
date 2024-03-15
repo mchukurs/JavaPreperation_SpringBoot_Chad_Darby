3efd00a02eb6baa9f8a48573277ed5268253f455
mc <m.cukurs1997@gmail.com>
2024-03-15 11:40:08 +0200

    qualifier

--- learning/src/main/java/com/chukurs/learning/controllers/CarController.java
+++ learning/src/main/java/com/chukurs/learning/controllers/CarController.java
@@ -3,6 +3,7 @@ package com.chukurs.learning.controllers;
 import com.chukurs.learning.services.CarService;
 import com.chukurs.learning.services.NannyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +12,17 @@ import java.util.List;
 @RestController
 
 public class CarController {
+
     private CarService carService;
 
     @Autowired
-    public void setCarService(CarService carService) {
+    public void setCarService(@Qualifier("superCarServiceImpl")CarService carService) {
         this.carService = carService;
     }
 
+//    public void CarController(@Qualifier("superCarServiceImpl") CarService carService) {
+//        this.carService = carService;
+//    }
     @GetMapping(path = "/car")
     public String getCar() {
         return carService.drive();
--- learning/src/main/java/com/chukurs/learning/services/impl/SuperCarServiceImpl.java
+++ learning/src/main/java/com/chukurs/learning/services/impl/SuperCarServiceImpl.java
@@ -1,11 +1,12 @@
 package com.chukurs.learning.services.impl;
 
 import com.chukurs.learning.services.CarService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SuperCarServiceImpl implements CarService {
 
+public class SuperCarServiceImpl implements CarService {
     @Override
     public String drive() {
         return "Drive super car";
