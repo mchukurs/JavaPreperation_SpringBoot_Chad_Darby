e981c0fc006238be05bb42c58bb1dcdc0ef239f4
mc <m.cukurs1997@gmail.com>
2024-03-19 11:36:52 +0200

    introduce error handling

--- 01-rest-crud/src/main/java/com/chukurs/simpleCrud/rest/controllers/StudentRestController.java
+++ 01-rest-crud/src/main/java/com/chukurs/simpleCrud/rest/controllers/StudentRestController.java
@@ -1,11 +1,12 @@
 package com.chukurs.simpleCrud.rest.controllers;
 
 import com.chukurs.simpleCrud.entity.Student;
+import com.chukurs.simpleCrud.rest.StudentErrorResponse;
+import com.chukurs.simpleCrud.rest.StudentNotFoundException;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,8 +30,23 @@ public class StudentRestController {
 
     @GetMapping(path = "/student/{studentId}")
     public Student getStudentById(@PathVariable int studentId) {
-
+        //checkign if student exsists
+        if (studentId >= studentList.size() || studentId < 0) {
+            //throw
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
         return studentList.get(studentId);
 
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setMessage(exc.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
