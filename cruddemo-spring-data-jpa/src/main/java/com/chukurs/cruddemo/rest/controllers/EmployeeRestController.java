b5c9d61fa2d311225857da14b6050c25a5e47a10
mc <m.cukurs1997@gmail.com>
2024-03-20 20:36:58 +0200

    replacing all by data-rest

--- cruddemo-spring-data-jpa/src/main/java/com/chukurs/cruddemo/rest/controllers/EmployeeRestController.java
+++ cruddemo-spring-data-jpa/src/main/java/com/chukurs/cruddemo/rest/controllers/EmployeeRestController.java
@@ -1,56 +1,56 @@
-package com.chukurs.cruddemo.rest.controllers;
-
-import com.chukurs.cruddemo.entity.Employee;
-import com.chukurs.cruddemo.services.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api")
-public class EmployeeRestController {
-    EmployeeService employeeService;
-
-    @Autowired
-    public EmployeeRestController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
-
-    @GetMapping(path = "/employees")
-    public List<Employee> findAll() {
-        return employeeService.findAll();
-    }
-
-    @GetMapping(path = "/employees/{id}")
-    public Employee findById(@PathVariable int id) {
-        Employee employee = employeeService.findById(id);
-        if (employee == null) {
-            throw new RuntimeException("Employee id not found " + id);
-        }
-        return employee;
-    }
-
-    @PostMapping(path = "/employees")
-    public Employee add(@RequestBody Employee employee) {
-        employee.setId(0);
-        System.out.println(employee.getEmail());
-        Employee dbEmployee = employeeService.save(employee);
-        return dbEmployee;
-    }
-
-    @PutMapping(path = "/employees")
-    public Employee update(@RequestBody Employee employee) {
-        return employeeService.save(employee);
-    }
-
-    @DeleteMapping(path = "/employees/{id}")
-    public String deleteById(@PathVariable int id) {
-        Employee employee = employeeService.findById(id);
-        if (employee == null) {
-            throw new RuntimeException("Employee id not found " + id);
-        }
-        employeeService.deleteById(id);
-        return "Employee ID: "+id+" is deleted.";
-    }
-}
+//package com.chukurs.cruddemo.rest.controllers;
+//
+//import com.chukurs.cruddemo.entity.Employee;
+//import com.chukurs.cruddemo.services.EmployeeService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class EmployeeRestController {
+//    EmployeeService employeeService;
+//
+//    @Autowired
+//    public EmployeeRestController(EmployeeService employeeService) {
+//        this.employeeService = employeeService;
+//    }
+//
+//    @GetMapping(path = "/employees")
+//    public List<Employee> findAll() {
+//        return employeeService.findAll();
+//    }
+//
+//    @GetMapping(path = "/employees/{id}")
+//    public Employee findById(@PathVariable int id) {
+//        Employee employee = employeeService.findById(id);
+//        if (employee == null) {
+//            throw new RuntimeException("Employee id not found " + id);
+//        }
+//        return employee;
+//    }
+//
+//    @PostMapping(path = "/employees")
+//    public Employee add(@RequestBody Employee employee) {
+//        employee.setId(0);
+//        System.out.println(employee.getEmail());
+//        Employee dbEmployee = employeeService.save(employee);
+//        return dbEmployee;
+//    }
+//
+//    @PutMapping(path = "/employees")
+//    public Employee update(@RequestBody Employee employee) {
+//        return employeeService.save(employee);
+//    }
+//
+//    @DeleteMapping(path = "/employees/{id}")
+//    public String deleteById(@PathVariable int id) {
+//        Employee employee = employeeService.findById(id);
+//        if (employee == null) {
+//            throw new RuntimeException("Employee id not found " + id);
+//        }
+//        employeeService.deleteById(id);
+//        return "Employee ID: "+id+" is deleted.";
+//    }
+//}
--- cruddemo-spring-data-jpa/src/main/java/com/chukurs/cruddemo/services/impl/EmployeeServiceImpl.java
+++ cruddemo-spring-data-jpa/src/main/java/com/chukurs/cruddemo/services/impl/EmployeeServiceImpl.java
@@ -1,50 +1,49 @@
-package com.chukurs.cruddemo.services.impl;
-
-import com.chukurs.cruddemo.dao.EmployeeRepository;
-import com.chukurs.cruddemo.entity.Employee;
-import com.chukurs.cruddemo.services.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class EmployeeServiceImpl implements EmployeeService {
-    EmployeeRepository employeeRepository;
-
-    @Autowired
-    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
-    }
-
-    @Override
-    public List<Employee> findAll() {
-        return employeeRepository.findAll();
-    }
-
-    @Override
-    public Employee findById(int id) {
-
-        Optional<Employee> result = employeeRepository.findById(id);
-        Employee theEmployee = null;
-        if (result.isPresent()) {
-            theEmployee = result.get();
-        } else {
-            throw new RuntimeException("Did not find employee with ID - " + id);
-        }
-
-        return theEmployee;
-    }
-
-    @Override
-    public Employee save(Employee employee) {
-        return employeeRepository.save(employee);
-    }
-
-    @Override
-    public void deleteById(int id) {
-        employeeRepository.deleteById(id);
-    }
-}
+//package com.chukurs.cruddemo.services.impl;
+//
+//import com.chukurs.cruddemo.dao.EmployeeRepository;
+//import com.chukurs.cruddemo.entity.Employee;
+//import com.chukurs.cruddemo.services.EmployeeService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class EmployeeServiceImpl implements EmployeeService {
+//    EmployeeRepository employeeRepository;
+//
+//    @Autowired
+//    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+//        this.employeeRepository = employeeRepository;
+//    }
+//    @Override
+//    public List<Employee> findAll() {
+//        return employeeRepository.findAll();
+//    }
+//
+//    @Override
+//    public Employee findById(int id) {
+//
+//        Optional<Employee> result = employeeRepository.findById(id);
+//        Employee theEmployee = null;
+//        if (result.isPresent()) {
+//            theEmployee = result.get();
+//        } else {
+//            throw new RuntimeException("Did not find employee with ID - " + id);
+//        }
+//
+//        return theEmployee;
+//    }
+//
+//    @Override
+//    public Employee save(Employee employee) {
+//        return employeeRepository.save(employee);
+//    }
+//
+//    @Override
+//    public void deleteById(int id) {
+//        employeeRepository.deleteById(id);
+//    }
+//}
