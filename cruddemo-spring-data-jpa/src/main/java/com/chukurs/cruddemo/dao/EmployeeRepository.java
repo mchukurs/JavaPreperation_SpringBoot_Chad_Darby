88966e129a855add57944022b67c46a80bf51bdb
mc <m.cukurs1997@gmail.com>
2024-03-21 12:31:33 +0200

    starter code from Chad

--- cruddemo-spring-data-jpa/src/main/java/com/chukurs/cruddemo/dao/EmployeeRepository.java
+++ cruddemo-spring-data-jpa/src/main/java/com/chukurs/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,8 @@ package com.chukurs.cruddemo.dao;
 
 import com.chukurs.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+@RepositoryRestResource(path="workers")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
