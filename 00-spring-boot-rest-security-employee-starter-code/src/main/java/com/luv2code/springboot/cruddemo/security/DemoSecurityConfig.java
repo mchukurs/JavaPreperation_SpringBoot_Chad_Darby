15a8605b39658e1b8d5caa099ef40e357d84bd77
mc <m.cukurs1997@gmail.com>
2024-03-22 14:55:22 +0200

    replace tables with custom tables

--- 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -18,9 +18,24 @@ import javax.sql.DataSource;
 //this overrides the properties file
 @Configuration
 public class DemoSecurityConfig {
+//    @Bean //jdbc support
+//    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
     @Bean //jdbc support
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id,pw,active from members where user_id=?"
+        );
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id,role from roles where user_id=?"
+        );
+        return jdbcUserDetailsManager;
     }
 
 
