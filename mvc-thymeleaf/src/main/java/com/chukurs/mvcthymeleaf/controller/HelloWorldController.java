b57abedcca06a7cd125ade0321f9d2d76d67c612
mc <m.cukurs1997@gmail.com>
2024-03-23 10:06:13 +0200

    adding Controller layer

--- mvc-thymeleaf/src/main/java/com/chukurs/mvcthymeleaf/controller/HelloWorldController.java
+++ mvc-thymeleaf/src/main/java/com/chukurs/mvcthymeleaf/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.chukurs.mvcthymeleaf.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.unbescape.xml.XmlEscape;
 
 @Controller
 public class HelloWorldController {
@@ -16,6 +19,16 @@ public class HelloWorldController {
     @RequestMapping(path = "/processForm")
     public String processForm() {
 
+        return "helloworld";
+    }
+
+    @RequestMapping(path = "/processFormVersionTwo")
+    public String processFormVersionTwo(HttpServletRequest request, Model model) {
+        String theName = request.getParameter("studentName");
+
+        theName = theName.toUpperCase();
+        String result = "YO " + theName;
+        model.addAttribute("message", result);
         return "helloworld";
     }
 
