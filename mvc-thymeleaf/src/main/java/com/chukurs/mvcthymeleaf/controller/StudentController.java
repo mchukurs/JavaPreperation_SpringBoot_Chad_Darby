b6445e959eded50e2dc84473149c34be96d6f69b
mc <m.cukurs1997@gmail.com>
2024-03-24 16:32:26 +0200

    thymeleaf each looping over list

--- mvc-thymeleaf/src/main/java/com/chukurs/mvcthymeleaf/controller/StudentController.java
+++ mvc-thymeleaf/src/main/java/com/chukurs/mvcthymeleaf/controller/StudentController.java
@@ -1,20 +1,27 @@
 package com.chukurs.mvcthymeleaf.controller;
 
 import com.chukurs.mvcthymeleaf.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping(path = "/showStudentForm")
+
     public String showForm(Model theModel) {
 
         Student newStudent = new Student();
         theModel.addAttribute("student", newStudent);
+        theModel.addAttribute("countries", countries);
         return "student-form";
     }
 
