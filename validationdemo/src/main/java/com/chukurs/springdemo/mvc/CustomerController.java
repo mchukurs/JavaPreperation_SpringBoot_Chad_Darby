872f607db4c192350b8942ce5b3c35c0557e0f46
mc <m.cukurs1997@gmail.com>
2024-03-25 16:55:27 +0200

    added validation for fields

---
+++ validationdemo/src/main/java/com/chukurs/springdemo/mvc/CustomerController.java
@@ -0,0 +1,35 @@
+package com.chukurs.springdemo.mvc;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+
+    @GetMapping(path = "/")
+    public String showForm(Model theModel) {
+        theModel.addAttribute("customer", new Customer());
+
+
+        return "customer-form";
+    }
+
+    @PostMapping(path = "/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer theCustomer,
+            BindingResult theBindingResult) {
+        if (theBindingResult.hasErrors()) {
+            System.out.println("has errors");
+            return "customer-form";
+        }
+else {
+            return "customer-confirmation";
+        }
+    }
+}
