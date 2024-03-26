fb92b996ef6c2ebbe12a80eedc475c58d62a3e93
mc <m.cukurs1997@gmail.com>
2024-03-26 09:48:30 +0200

    add postal with regex validation

--- validationdemo/src/main/java/com/chukurs/springdemo/mvc/Customer.java
+++ validationdemo/src/main/java/com/chukurs/springdemo/mvc/Customer.java
@@ -1,11 +1,10 @@
 package com.chukurs.springdemo.mvc;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
     @Min(value = 0, message = "must be greater than or equal to 0")
     @Max(value = 10, message = "must be less than or equal to 10")
     private int freePasses;
@@ -14,6 +13,14 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName = "";
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
     public int getFreePasses() {
         return freePasses;
