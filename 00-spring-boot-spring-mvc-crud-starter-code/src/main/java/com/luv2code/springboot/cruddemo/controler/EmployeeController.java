fb387c0c3c93d4757703b4c308967f2a00638150
mc <m.cukurs1997@gmail.com>
2024-03-27 14:07:39 +0200

    update option added per employee

--- 00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controler/EmployeeController.java
+++ 00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controler/EmployeeController.java
@@ -4,10 +4,7 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +32,16 @@ public class EmployeeController {
     public String showFormForAdd(Model theModel) {
         Employee theEmployee = new Employee();
         theModel.addAttribute("employee", theEmployee);
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+        //get the employee from DB via service
+        Employee theEmployee = employeeService.findById(theId);
+        //set "employee" to model (the id will be hidden, but we can work with it)
+        theModel.addAttribute("employee", theEmployee);
+
         return "employees/employee-form";
     }
 
