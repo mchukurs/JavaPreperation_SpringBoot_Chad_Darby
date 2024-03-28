7bef165d7c3ad3b781f4c723054b28c2d10095b6
mc <m.cukurs1997@gmail.com>
2024-03-28 15:39:55 +0200

    add home page

---
+++ demosecurity/src/main/java/com/chukurs/springboot/demosecurity/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.chukurs.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class DemoController {
+    @GetMapping("/")
+    public String showHome() {
+        return "home";
+    }
+}
