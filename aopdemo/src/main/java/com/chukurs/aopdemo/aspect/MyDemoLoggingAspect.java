5f16775f998f51a91b06d53a419c38715737bd99
mc <m.cukurs1997@gmail.com>
2024-04-12 09:20:28 +0300

    point cut expression on return type

--- aopdemo/src/main/java/com/chukurs/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aopdemo/src/main/java/com/chukurs/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-    @Before("execution(* add*())")
+    @Before("execution(boolean add*())")
     public void beforeAddAccountAdvice() {
         System.out.println("This code was ran before");
     }
