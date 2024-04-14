0f19b61b278e8b77addaaefed3555810608fd1b9
mc <m.cukurs1997@gmail.com>
2024-04-14 12:34:23 +0300

    accessing arguments and method signature via JoinPoint

--- aopdemo/src/main/java/com/chukurs/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ aopdemo/src/main/java/com/chukurs/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -1,9 +1,14 @@
 package com.chukurs.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.aspectj.weaver.patterns.ArgsPointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -12,7 +17,15 @@ public class MyApiAnalyticsAspect {
 
 
     @Before("com.chukurs.aopdemo.aspect.ChukursAOPExpressions.forDAOPackage()")
-    public void performingApiAnalytics() {
-        System.out.println("\nThis code performingApiAnalytics");
+    public void performingApiAnalytics(JoinPoint theJoinPoint) {
+
+        System.out.println("\nThis code performingApiAnalytics and has JoinPoint with \n");
+
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+        System.out.println("Method Signature: "+ methodSignature);
+
+        Object[] args = theJoinPoint.getArgs();
+        System.out.println("Arguments: "+ Arrays.toString(args));
+
     }
 }
